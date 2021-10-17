dd6181a573b0718143e41e2c9c9e7580b20871fc
exgs <yunafrica@gmail.com>
2021-10-17 20:08:47 +0900

    Delete: DBconnect.cs 소스코드 삭제
    legacy한 함수들를 모두 getDBData 클래스의 static method로 변경시킴

--- coU/Assets/Scene/Scripts/Scene/SearchSceneManager.cs
+++ coU/Assets/Scene/Scripts/Scene/SearchSceneManager.cs
@@ -68,7 +68,7 @@ public class SearchSceneManager : MonoBehaviour
             DestroyImmediate(items[i]);
         string query = "Select * from Stores where name like '%" + inputText.Trim() + "%'";
         query += "group by name order by name ASC";
-        List<Stores> stores = DBConnect.CustomExcuteQuery(query);
+        List<Stores> stores = getDBData.getStoresData(query);
         items = new GameObject[stores.ToArray().Length];
         print("items number = " + items.Length);
         for (int i = 0; i < items.Length; i++)
@@ -84,7 +84,8 @@ public class SearchSceneManager : MonoBehaviour
             DestroyImmediate(results[i]);
         string query = "Select * from Stores where name like '%" + inputText.Trim() + "%'";
         query += "group by name order by name ASC";
-        List<Stores> stores = DBConnect.CustomExcuteQuery(query);
+
+        List<Stores> stores = getDBData.getStoresData(query);
         results = new GameObject[stores.ToArray().Length];
         for (int i = 0; i < results.Length; i++)
         {
