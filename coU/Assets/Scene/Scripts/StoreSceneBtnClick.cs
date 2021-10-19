748d79bb706191e1060f2a4e48b271df5a96a7fa
exgs <yunafrica@gmail.com>
2021-10-19 21:28:25 +0900

    Bug: syntax error

--- coU/Assets/Scene/Scripts/StoreSceneBtnClick.cs
+++ coU/Assets/Scene/Scripts/StoreSceneBtnClick.cs
@@ -27,12 +27,12 @@ public class StoreSceneBtnClick : MonoBehaviour
 
     public void ShareBtnOnClick()
     {
+        string subject = "42_coU";
 		string scene = "StoreScene";
 		string name = StoreSceneManager.storeName;
         string categoryMain = StoreSceneManager.categoryMain;
         string categorySub = StoreSceneManager.categorySub;
-
-    string uri = string.Format("https://github.com/exgs/yunsleeMap?scene={0}&name={1}&categoryMain={2}&categorySub={3}",
+        string uri = string.Format("https://github.com/exgs/yunsleeMap?scene={0}&name={1}&categoryMain={2}&categorySub={3}",
                             scene, name, categoryMain, categorySub);
         #if UNITY_ANDROID && !UNITY_EDITOR
 		    AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
@@ -40,7 +40,7 @@ public class StoreSceneBtnClick : MonoBehaviour
 		    intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
 		    intentObject.Call<AndroidJavaObject>("setType", "text/plain");
 		    intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_SUBJECT"), subject);
-		    intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), body);
+		    intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), uri);
 		    AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
 		    AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
 		    AndroidJavaObject jChooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intentObject, "Share Via");
