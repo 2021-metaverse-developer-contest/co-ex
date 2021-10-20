a63672c62a90b66dfd82a3746ff5aedb91877597
201611099 <hyojlee@ihyojeong-ui-MacBookPro.local>
2021-10-20 23:48:25 +0900

    Temp: World Space 캔버스 클릭 이벤트 진행중

--- coU/Assets/Scene/Scripts/MaxstBtnClick.cs
+++ coU/Assets/Scene/Scripts/MaxstBtnClick.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class MaxstBtnClick : MonoBehaviour
@@ -14,5 +16,16 @@ public class MaxstBtnClick : MonoBehaviour
     public void CategoryBtnOnClick()
     {
         SceneManager.LoadScene("AllCategoryScene");
+    }
+
+    public void StoreOnClick()
+    {
+        Debug.Log("Store Info Click");
+        GameObject clickObj = EventSystem.current.currentSelectedGameObject;
+
+        TextMeshProUGUI storeName = clickObj.transform.Find("Panel_StoreInfo").GetComponentInChildren<TextMeshProUGUI>();
+        //TextMeshProUGUI storeName = clickObj.transform.Find("Panel_StoreInfo").Find("TMP_StoreName").GetComponent<TextMeshProUGUI>();
+        SceneManager.LoadScene("StoreScene");
+        StoreSceneManager.storeName = storeName.text;
     }
 }
