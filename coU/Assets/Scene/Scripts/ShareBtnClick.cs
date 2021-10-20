28c1eda1f373c86a52684e5efe9e5265ccc963e4
exgs <yunafrica@gmail.com>
2021-10-20 12:29:33 +0900

    Bug: 접속하는 github웹페이지를 수정함

--- coU/Assets/Scene/Scripts/ShareBtnClick.cs
+++ coU/Assets/Scene/Scripts/ShareBtnClick.cs
@@ -11,19 +11,26 @@ public class ShareBtnClick : MonoBehaviour
 		string name = StoreSceneManager.storeName;
 		string categoryMain = StoreSceneManager.categoryMain;
 		string categorySub = StoreSceneManager.categorySub;
-		string uri = string.Format("https://github.com/exgs/yunsleeMap?scene={0}&name={1}&categoryMain={2}&categorySub={3}",
+		string uri = string.Format("https://exgs.github.io/yunsleeMap/urlScheme.html?scene={0}&name={1}&categoryMain={2}&categorySub={3}",
 							scene, name, categoryMain, categorySub);
+		print(subject);
+		print(uri);
 #if UNITY_ANDROID && !UNITY_EDITOR
-		    AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
-		    AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
-		    intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
-		    intentObject.Call<AndroidJavaObject>("setType", "text/plain");
-		    intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_SUBJECT"), subject);
-		    intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), uri);
-		    AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-		    AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
-		    AndroidJavaObject jChooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intentObject, "Share Via");
-		    currentActivity.Call("startActivity", jChooser);
+		AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
+		AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
+		intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
+		intentObject.Call<AndroidJavaObject>("setType", "text/plain");
+		intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_SUBJECT"), subject);
+		intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), uri);
+		AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+		AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+		AndroidJavaObject jChooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intentObject, "Share Via");
+		currentActivity.Call("startActivity", jChooser);
+#elif UNITY_IOS && !UNITY_EDITOR
+		Application.OpenURL(uri);
+#elif UNITY_EDITOR
+		Application.OpenURL(uri);
 #endif
+		//Application.OpenURL("https://www.naver.com");
 	}
 }
