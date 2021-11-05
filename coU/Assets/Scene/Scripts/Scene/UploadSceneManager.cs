e334d7683e97596ce176f9e42c92a445c551ce48
hyojlee <42.4.hyojlee@gmail.com>
2021-11-05 11:06:57 +0900

    Temp: UploadScene_v.0.1

---
+++ coU/Assets/Scene/Scripts/Scene/UploadSceneManager.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class UploadSceneManager : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
