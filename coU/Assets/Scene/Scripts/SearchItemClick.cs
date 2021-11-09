f8f31d37ead3e12f645e1ed603509c616247a7d2
hyojlee <yan04121@naver.com>
2021-11-09 16:20:24 +0900

    Modified: SearchScene itemList 클릭 이벤트 수정 SelectStoreScene과 동일한 방식으로 동작하도록 수정 Closes: #72

--- coU/Assets/Scene/Scripts/SearchItemClick.cs
+++ coU/Assets/Scene/Scripts/SearchItemClick.cs
@@ -4,9 +4,20 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SearchItemClick : MonoBehaviour
 {
+    public void ListItemOnClick()
+    {
+        GameObject cur = EventSystem.current.currentSelectedGameObject;
+        TMP_InputField search = GameObject.Find("InputTMP_Search").GetComponent<TMP_InputField>();
+
+        search.text = cur.GetComponentInChildren<TextMeshProUGUI>().text;
+        Button searchBtn = GameObject.Find("Btn_Search").GetComponent<Button>();
+        searchBtn.onClick.Invoke();
+    }
+
     public void ResultItemOnClick()
     {
         GameObject cur = EventSystem.current.currentSelectedGameObject;
@@ -14,16 +25,6 @@ public class SearchItemClick : MonoBehaviour
         StoreSceneManager.storeName = cur.transform.Find("TMP_Result").GetComponent<TextMeshProUGUI>().text;
         StoreSceneManager.categorySub = "";
         Stack.Instance.Push(new SceneInfo(SceneManager.GetActiveScene().buildIndex, SearchSceneManager.searchStr, true));
-        SceneManager.LoadScene("StoreScene");
-    }
-
-    public void ListItemOnClick()
-    {
-        GameObject cur = EventSystem.current.currentSelectedGameObject;
-
-        StoreSceneManager.storeName = cur.transform.Find("TMP_List").GetComponent<TextMeshProUGUI>().text;
-        StoreSceneManager.categorySub = "";
-        Stack.Instance.Push(new SceneInfo(SceneManager.GetActiveScene().buildIndex, StoreSceneManager.storeName, true));
         SceneManager.LoadScene("StoreScene");
     }
 }
