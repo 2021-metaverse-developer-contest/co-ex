a1d02d37c21dcaa69b7413a60f9a0c439c2a359a
exgs <yunafrica@gmail.com>
2021-11-12 07:37:26 +0900

    코드구현에 대한 대략적인 구조를 작성함
    저장버튼이 없는 UploadScene에서 이미지삭제(detachimage), 이미지 업로드(attachImage), 이미지 순서이동(changeOrderImage) 세가지 동작이 있다. 각각의 경우를 메소드로 만들고 구현을 생각해보았음

--- coU/Assets/Scene/Scripts/Scene/UploadSceneManager.cs
+++ coU/Assets/Scene/Scripts/Scene/UploadSceneManager.cs
@@ -28,7 +28,7 @@ public class UploadSceneManager : MonoBehaviour
         Screen.orientation = ScreenOrientation.Portrait;
         GameObject.Find("TMP_StoreName").GetComponent<TextMeshProUGUI>().text = storeName;
         //LoadCoroutine();
-        readStoreImgsCorutine();
+        init();
     }
 
 	private void Update()
@@ -130,4 +130,36 @@ public class UploadSceneManager : MonoBehaviour
         }
     }
 
+    public void init()
+    {
+        // 1. DAO 클래스에 담기 + 클래스 sortOrder 순으로 정렬
+        readStoreImgsCorutine();
+        //ListStoreImgs.Sort() => order로 정렬되도록 함수짜기!
+        // 2. 클래스의 길이가 0이 아니라면, 첫번째 sortOrder에 있는 이미지 뿌려주기 void Load()로
+    }
+    public void attachImage()
+    {
+        // 0. StoreImgs객체를 만드는게 처음 할일
+		//      StoreImgs(storeName, imgType, sortOrder)에서 imgType만 구해서 넣어준다.(storeName과 sortOrder는 이미 정해져있음)
+        // 1. image List UI 하나 맨 아래 추가
+        // 2. Realtime DB에 저장하기
+        // 3. storage에 업로드하ㄱ
+    }
+
+    public void detachImage()
+    {
+        // 1. DeleteItemBtnOnClick을 누르면, 하이어라키에서 해당 리스트가 지워진다.
+        // 2. Text에 적혀있었던 imgName과 List.Remove()를 이용하여 이미지리스트에서 해당 부분 삭제
+        // 3. DB에서 해당 부분만 삭제하기 => "1_계절밥상" Key값을 객체 변수로부터 알 수 가 없음.
+		// 따라서 현재로썬 일딴은 다 지우고, 리스트로 저장되어있는 storeImgs로 갱신하는 것으로 하기
+        // 4. storage에서도 imgPath를 이용하여 삭제
+    }
+
+    // 다른 함수와 달리 Storage를 변경하지 않아도 된다.
+    public void changeOrderImage()
+	{
+        // 1. image List를 이동시키는 것을 감지하면,
+        // 2. List의 순서를 변경하고,
+        // 3. DB에 저장시킨다.
+    }
 }
