0425e81ae4ae4f05769a8095f645c702a539449a
hyojlee <yan04121@naver.com>
2021-11-14 21:11:08 +0900

    Feat: StoreScene Image auto scroll + MenuScene에서 "카테고리 안내" -> "매장 안내"로 수정

--- coU/Assets/Scene/Scripts/ImgScrolling.cs
+++ coU/Assets/Scene/Scripts/ImgScrolling.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,28 +7,60 @@ using UnityEngine.UI;
 public class ImgScrolling : MonoBehaviour
 {
 	public RectTransform content; //움직일 오브젝트
-	public int count; //나눠야할 값
+	Button nextBtn;
+	private int count; //나눠야할 값
 	private float pos; //content의 LocalPosition
 	private float movepos; //움직일 값
 	private bool isScroll = false; //움직여야하는 지 구별
+	private float imgWidth;
+	float nextTime;
+	float timeLeft = 5.0f;
+
+	private void Awake()
+	{
+		nextBtn = GameObject.Find("Btn_Next").GetComponent<Button>();	
+	}
 
 	// Start is called before the first frame update
 	void Start()
 	{
+		nextTime = Time.time + timeLeft;
+		imgWidth = GameObject.Find("Img_Logo").GetComponent<RectTransform>().rect.width;
+		Debug.Log($"Start's imgWidth {imgWidth}");
+		foreach (Transform c in content.transform)
+		{
+			if (c.gameObject.activeSelf)
+				count++;
+		}
+		//Debug.Log($"Start's child count {count}");
+		//Debug.Log($"Start's movepos, content.rect.xMax {movepos.ToString()}, {content.rect.xMax}");
+		//Debug.Log($"Start's rect.width {content.rect.width}");
+
+		/*
+		 * movepos = content.rect.xMax - content.rect.xMax / count
+		 *         = content.rect.xMax * (count - 1) /count
+		 * content.rect.xMax = imgWidth * count / 2
+		 * => movepos = 1406 * (count - 1) / 2
+		 */
+		movepos = imgWidth * (count - 1) / 2;
+		while (Vector2.Distance(content.localPosition, new Vector2(movepos, 0)) >= 0.1f)
+			content.localPosition = Vector2.Lerp(content.localPosition, new Vector2(movepos, 0), Time.deltaTime * 5);
 		pos = content.localPosition.x;
-		movepos = content.rect.xMax - content.rect.xMax / count;
-		Debug.Log($"Start's movepos, content.rect.xMax {movepos.ToString()}, {content.rect.xMax}");
-		Debug.Log($"Start's rect.width {content.rect.width}");
-		//while (Vector2.Distance(content.localPosition, new Vector2(2812, 0)) >= 0.1f)
-		//    content.localPosition = Vector2.Lerp(content.localPosition, new Vector2(2812, 0), Time.deltaTime * 5);
 	}
 
 	public void Right()
 	{
-		Debug.Log($"Start's no   movepos, content.rect.xMax {movepos.ToString()}, {content.rect.xMax}");
-		Debug.Log($"Start's no   rect.width {content.rect.width}");
-		if (content.rect.xMin + content.rect.xMax / count == movepos)
+		Debug.Log($"Start's right   movepos			  {movepos.ToString()}");
+		Debug.Log($"Start's right   content.rect.xMax {content.rect.xMax}");
+		Debug.Log($"Start's right   content.rect.xMin {content.rect.xMin}");
+		Debug.Log($"currentLocalPosition {content.localPosition.ToString()}");
+		Debug.Log($"Start's right {content.rect.xMin + content.rect.xMax / count}");
+		if (content.rect.xMin + content.rect.xMax / count  == Math.Round(movepos))
 		{
+			movepos = imgWidth * (count - 1) / 2;
+			while (Vector2.Distance(content.localPosition, new Vector2(movepos, 0)) >= 0.1f)
+				content.localPosition = Vector2.Lerp(content.localPosition, new Vector2(movepos, 0), Time.deltaTime * 5);
+			pos = content.localPosition.x;
 			Debug.Log("Don't Move Right"); //Left로 가야됨
 		}
 		else
@@ -41,10 +74,17 @@ public class ImgScrolling : MonoBehaviour
 
 	public void Left()
 	{
-		Debug.Log($"Start's left   movepos, content.rect.xMax {movepos.ToString()}, {content.rect.xMax}");
-		Debug.Log($"Start's left   rect.width {content.rect.width}");
-		if (content.rect.xMax - content.rect.xMax / count == movepos)
+		Debug.Log($"Start's left   movepos			  {Math.Round(movepos).ToString()}");
+		Debug.Log($"Start's left   content.rect.xMax {content.rect.xMax}");
+		Debug.Log($"Start's left   content.rect.xMin {content.rect.xMin}");
+		Debug.Log($"currentLocalPosition {content.localPosition.ToString()}");
+		Debug.Log($"Start's left {content.rect.xMax - content.rect.xMax / count}");
+		if (content.rect.xMax - content.rect.xMax / count == Math.Round(movepos))
 		{
+			movepos = imgWidth * (count - 1) / 2 * -1;
+			while (Vector2.Distance(content.localPosition, new Vector2(movepos, 0)) >= 0.1f)
+				content.localPosition = Vector2.Lerp(content.localPosition, new Vector2(movepos, 0), Time.deltaTime * 5);
+			pos = content.localPosition.x;
 			Debug.Log("Don't Move Left"); //Right로 가야됨
 		}
 		else
@@ -58,20 +98,25 @@ public class ImgScrolling : MonoBehaviour
 
 	IEnumerator Scroll()
 	{
+		nextTime = Time.time + timeLeft;
 		while (isScroll)
 		{
 			content.localPosition = Vector2.Lerp(content.localPosition, new Vector2(movepos, 0), Time.deltaTime * 5);
 			if (Vector2.Distance(content.localPosition, new Vector2(movepos, 0)) < 0.1f)
 			{
+				Debug.Log($"Start's Coroutine {content.localPosition}");
 				isScroll = false;
 			}
 			yield return null;
 		}
 	}
 
-	// Update is called once per frame
-	void Update()
+	private void Update()
 	{
-
+		if (Time.time > nextTime)
+		{
+			nextTime = Time.time + timeLeft;
+			Right();
+		}
 	}
 }
