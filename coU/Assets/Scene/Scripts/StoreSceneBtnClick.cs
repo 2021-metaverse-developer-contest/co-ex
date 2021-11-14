e899c8eff3afed9dab5f54c356bfd531ed959ad1
exgs <yunafrica@gmail.com>
2021-11-14 13:03:15 +0900

    Edit: naviStart => chkNaviBtnClick
    가독성을 위한 변수명 수정, StoreScene에서 버튼을 눌렀을 때, true가 되는 변수의 변수명을 수정함

--- coU/Assets/Scene/Scripts/StoreSceneBtnClick.cs
+++ coU/Assets/Scene/Scripts/StoreSceneBtnClick.cs
@@ -31,7 +31,7 @@ public class StoreSceneBtnClick : MonoBehaviour
         MaxstSceneManager.naviStoreName = StoreSceneManager.storeName;
         MaxstSceneManager.naviStoreCategorySub = StoreSceneManager.categorySub;
         MaxstSceneManager.naviStoreFloor = StoreSceneManager.floor;
-        MaxstSceneManager.naviStart = true;
+        MaxstSceneManager.chkNaviBtnClick = true;
         //Stack.Instance.Push(new SceneInfo(SceneManager.GetActiveScene().buildIndex, StoreSceneManager.storeName, StoreSceneManager.categorySub));
         Stack.Instance.Clear(); // MaxstScene에서는 언제나 바로 종료되므로
         SceneManager.LoadScene("MaxstScene");
