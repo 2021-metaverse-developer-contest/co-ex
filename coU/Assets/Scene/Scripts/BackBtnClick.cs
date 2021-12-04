67b4784ac0da7411cab97851e8f12f47421cca05
exgs <yunafrica@gmail.com>
2021-12-04 19:15:16 +0900

    Feat: 11개의 씬의 BackBtn을 하나의 소스 코드로 복붙
    1. 단, StoreScene에서 콘텐츠(이미지)가 자동으로 넘어가는 코드 부분은 복붙하지 않음
    2.  단, MaxstScene에서 navicheck하는 코드 부분은 복붙하지 않음

---
+++ coU/Assets/Scene/Scripts/BackBtnClick.cs
@@ -0,0 +1,191 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackBtnClick : MonoBehaviour
+{
+    public class SceneName
+	{
+        public const string AllCategoryScene = nameof(AllCategoryScene);
+        public const string FirstScene = nameof(FirstScene);
+        public const string LoadingScene = nameof(LoadingScene);
+        public const string LoginScene = nameof(LoginScene);
+        public const string MaxstScene = nameof(MaxstScene);
+        public const string MenuScene = nameof(MenuScene);
+        public const string RegisterScene = nameof(RegisterScene);
+        public const string SearchScene = nameof(SearchScene);
+        public const string StoreListScene = nameof(StoreListScene);
+        public const string StoreScene = nameof(StoreScene);
+        public const string UploadScene = nameof(UploadScene);
+    }
+
+    private int backCount = 0;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // 12.4 Storescene? MaxstScene ? ? ???? ??? ????? ??? ??
+    void ResetBackCount()
+    {
+        backCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (this.gameObject.scene.name)
+		{
+            case SceneName.AllCategoryScene:
+                if (Input.GetKeyDown(KeyCode.Escape) && SceneManager.sceneCount < 2)
+				{
+                    Stack.Instance.Clear();
+                    SceneManager.LoadScene("MaxstScene");
+                }
+                break;
+            case SceneName.FirstScene:
+                break;
+            case SceneName.LoadingScene:
+                break;
+            case SceneName.LoginScene:
+                if (Input.GetKeyDown(KeyCode.Escape))
+                    SceneManager.UnloadSceneAsync("LoginScene");
+                break;
+            case SceneName.MaxstScene:
+                // 2021/10/18 hyojlee
+                // MaxstScene?? ???? ?? ?? ? ? ???? ??
+                // ? ? ??? ???? ?? ?????? ??? ??? ??? ?
+                if (Input.GetKeyDown(KeyCode.Escape) && SceneManager.sceneCount < 2)
+                {
+                    backCount++;
+                    if (!IsInvoking("ResetBackCount"))
+                        Invoke("ResetBackCount", 1.0f);
+                    if (backCount == 2)
+                    {
+                        CancelInvoke("ResetBackCount");
+                        Application.Quit();
+                        #if !UNITY_EDITOR
+                            System.Diagnostics.Process.GetCurrentProcess().Kill();
+                        #endif
+                    }
+                        #if UNITY_EDITOR
+                                            Debug.Log("? ? ? ???? ?????.");
+                        #elif UNITY_ANDROID
+			                                            Toast.ShowToastMessage("? ? ? ???? ?????.", Toast.Term.shortTerm);
+                        #endif
+                }
+                break;
+            case SceneName.MenuScene:
+                if (Input.GetKeyDown(KeyCode.Escape))
+                    SceneManager.UnloadSceneAsync("MenuScene");
+                break;
+            case SceneName.RegisterScene:
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    if (GameObject.Find("Panel_SelectStore") != null)
+                        GameObject.Find("Panel_SelectStore").SetActive(false);
+                    else
+                        GameObject.Find("Canvas_PopRegister").transform.Find("Panel_PopCloseRegister").gameObject.SetActive(true);
+                }
+                break;
+            case SceneName.SearchScene:
+                if (Input.GetKeyDown(KeyCode.Escape) && SceneManager.sceneCount < 2)
+                    BackBtnOnClick();
+                void BackBtnOnClick()
+                {
+                    SceneInfo before = Stack.Instance.Pop();
+                    string beforePath = SceneUtility.GetScenePathByBuildIndex(before.beforeScene);
+
+                    if (beforePath.Contains("StoreScene"))
+                    {
+                        StoreSceneManager.storeName = before.storeName;
+                        StoreSceneManager.categorySub = before.categorySub;
+                    }
+                    else if (beforePath.Contains("StoreListScene"))
+                    {
+                        StoreListSceneManager.categorySub = before.categorySub;
+                    }
+                    else //MaxstScene?? ??, AllCategoryScene?? ?? ?? ???? ?.
+                        Stack.Instance.Clear();
+                    SceneManager.LoadScene(before.beforeScene);
+                }
+                break;
+            case SceneName.StoreListScene:
+                if (Input.GetKeyDown(KeyCode.Escape) && SceneManager.sceneCount < 2)
+                    SceneManager.LoadScene("AllCategoryScene");
+                break;
+            case SceneName.StoreScene:
+                if (Input.GetKeyDown(KeyCode.Escape) && (SceneManager.sceneCount == 1
+                   || (SceneManager.sceneCount == 2 && SceneManager.GetActiveScene().name == "MaxstScene")))
+                {
+                    if (GameObject.Find("Panel_ChooseWhole") != null)
+                    {
+                        GameObject.Find("Panel_ChooseWhole").SetActive(false);
+                    }
+                    else if (Stack.Instance.Count() > 0)
+                    {
+                        BackBtnOnClick2();
+                        void BackBtnOnClick2()
+                        {
+                            if (Stack.Instance.Count() == 0)
+                            {
+                                SceneManager.LoadScene("AllCategoryScene");
+                                return;
+                            }
+                            SceneInfo before = Stack.Instance.Pop();
+                            string beforePath = SceneUtility.GetScenePathByBuildIndex(before.beforeScene);
+                            if (beforePath.Contains("MaxstScene"))
+                            {
+                                Stack.Instance.Clear();
+                                SceneManager.UnloadSceneAsync("StoreScene");
+                            }
+                            else
+                            {
+                                if (beforePath.Contains("SearchScene"))
+                                    SearchSceneManager.searchStr = before.storeName;
+                                else if (beforePath.Contains("StoreListScene"))
+                                    StoreListSceneManager.categorySub = before.categorySub;
+                                else //MaxstScene?? ??, AllCategoryScene?? ?? ?? ???? ?.
+                                    Stack.Instance.Clear();
+                                SceneManager.LoadScene(before.beforeScene);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        backCount++;
+                        if (!IsInvoking("ResetBackCount"))
+                            Invoke("ResetBackCount", 1.0f);
+                        if (backCount == 2)
+                        {
+                            CancelInvoke("ResetBackCount");
+                            Application.Quit();
+                            #if !UNITY_EDITOR
+	                            System.Diagnostics.Process.GetCurrentProcess().Kill();
+                            #endif
+                        }
+                        #if UNITY_EDITOR
+                                                Debug.Log("? ? ? ???? ?????.");
+                        #elif UNITY_ANDROID
+			                            Toast.ShowToastMessage("? ? ? ???? ?????.", Toast.Term.shortTerm);
+                        #endif
+                    }
+
+                }
+                break;
+            case SceneName.UploadScene:
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    GameObject.Find("Canvas_UploadPop").transform.Find("Panel_PopCloseUpload").gameObject.SetActive(true);
+                }
+                break;
+            default:
+                print($"{this.gameObject.scene.name}? ???? ?? ????.");
+                break;
+        }
+    }
+}
