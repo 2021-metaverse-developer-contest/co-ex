2621d9b103ebaa447a7255e14d4177731e01cf8f
exgs <yunslee@student.42seoul.kr>
2022-04-16 17:43:25 +0900

    Delete Assets folder

--- coU/Assets/GPM/Common/GpmCommon.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Gpm.Common
-{
-    public static class GpmCommon
-    {
-        public const string VERSION = "2.0.6";
-        public static bool DebugLogEnabled { get; set; }
-    }
-}
